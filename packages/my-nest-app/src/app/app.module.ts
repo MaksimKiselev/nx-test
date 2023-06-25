180d88eeaf0ea0536c927bcef64dbc6032690789
MaksimKiselev <maksim.kiselev@segmentstream.com>
2023-06-25 18:23:58 +0300

    format

--- packages/my-nest-app/src/app/app.module.ts
+++ packages/my-nest-app/src/app/app.module.ts
@@ -1,14 +1,13 @@
-import {Module} from '@nestjs/common';
+import { Module } from '@nestjs/common';
 
-import {MyNestLibModule} from "@nx-test/my-nest-lib";
+import { MyNestLibModule } from '@nx-test/my-nest-lib';
 
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [MyNestLibModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
